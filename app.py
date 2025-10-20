24073cf5ad9734481a3728b0ed918755cff2a31b
Nitish Kumar <nitish84kr@gmail.com>
2025-10-20 14:51:03 +0530

    add image to the home page

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Campusx")
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.image(r"182502866_a99f.jpg")
+
+with col2:
+    st.header("CampusX is an Indian ed-tech company founded by Nitish Singh that offers online courses and mentorship programs in data science and machine learning. The company provides free educational content on its YouTube channel, with more in-depth, paid programs available on its website.")    
