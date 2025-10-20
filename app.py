e0a2fa5bdd0065791dbe3a37456849e355b84951
Nitish Kumar <nitish84kr@gmail.com>
2025-10-20 17:25:32 +0530

    replace header image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Campusx")
 col1,col2=st.columns(2)
 
 with col1:
-    st.image(r"182502866_a99f.jpg")
+    st.image(r"Screen+Shot+2023-08-17+at+3.09.40+PM.webp")
 
 with col2:
     st.header("CampusX is an Indian ed-tech company founded by Nitish Singh that offers online courses and mentorship programs in data science and machine learning. The company provides free educational content on its YouTube channel, with more in-depth, paid programs available on its website.")    
